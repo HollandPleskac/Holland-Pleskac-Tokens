8d94bcf8c9c76fd595a8cadb27d6b5accbffb012
Holland Pleskac <hollandpleskac@gmail.com>
2021-08-06 20:29:51 -0700

    placeholder pages

--- pages/testing/greeter.js
+++ pages/testing/greeter.js
@@ -1,5 +1,18 @@
 // Commented out - the Greeter import causes an error when deploying to vercel
 
+import React from 'react'
+
+const PagePlaceHolder = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default PagePlaceHolder
+
+
 // import React, { useState } from 'react'
 // import { ethers } from 'ethers'
 // import Greeter from '../../artifacts/contracts/Greeter.sol/Greeter.json'
