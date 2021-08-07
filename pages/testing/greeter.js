a95419e7d849a8339d733263929e992aba584e3b
Holland Pleskac <hollandpleskac@gmail.com>
2021-08-06 20:20:33 -0700

    vercel deploy

--- pages/testing/greeter.js
+++ pages/testing/greeter.js
@@ -1,58 +1,60 @@
-import React, { useState } from 'react'
-import { ethers } from 'ethers'
-import Greeter from '../../artifacts/contracts/Greeter.sol/Greeter.json'
+// Commented out - the Greeter import causes an error when deploying to vercel
 
-const greeterAddress = '0x5fbdb2315678afecb367f032d93f642f64180aa3'
+// import React, { useState } from 'react'
+// import { ethers } from 'ethers'
+// import Greeter from '../../artifacts/contracts/Greeter.sol/Greeter.json'
 
-const HomePage = () => {
-  const [greeting, setGreetingValue] = useState('')
+// const greeterAddress = '0x5fbdb2315678afecb367f032d93f642f64180aa3'
 
-  const requestAccount = async () => {
-    console.log('requesting meta mask accounts')
-    await window.ethereum.request({ 'method': 'eth_requestAccounts' }) // prompt user to connect meta mask account if not connected (returs accounts array)
-  }
+// const HomePage = () => {
+//   const [greeting, setGreetingValue] = useState('')
 
-  const fetchGreeting = async () => {
-    if (typeof window.ethereum !== undefined) {
-      const provider = new ethers.providers.Web3Provider(window.ethereum)
-      const contract = new ethers.Contract(greeterAddress, Greeter.abi, provider)
-      try {
-        const data = await contract.greet()
-        console.log('data: ', data)
-      } catch (err) {
-        console.log('error: ', err)
-      }
-    }
-  }
+//   const requestAccount = async () => {
+//     console.log('requesting meta mask accounts')
+//     await window.ethereum.request({ 'method': 'eth_requestAccounts' }) // prompt user to connect meta mask account if not connected (returs accounts array)
+//   }
 
-  const setGreeting = async () => {
-    if (!greeting) return
-    if (typeof window.ethereum !== undefined) {
-      await requestAccount()
-      const provider = new ethers.providers.Web3Provider(window.ethereum)
-      const signer = provider.getSigner()
-      const contract = new ethers.Contract(greeterAddress, Greeter.abi, signer) // create a transaction with signer (pass in signer instead of provider like above)
-      const transaction = await contract.setGreeting(greeting)
-      setGreetingValue('')
-      await transaction.wait() // wait for transaction to confirm on blockchain
-      fetchGreeting()
-    }
-  }
+//   const fetchGreeting = async () => {
+//     if (typeof window.ethereum !== undefined) {
+//       const provider = new ethers.providers.Web3Provider(window.ethereum)
+//       const contract = new ethers.Contract(greeterAddress, Greeter.abi, provider)
+//       try {
+//         const data = await contract.greet()
+//         console.log('data: ', data)
+//       } catch (err) {
+//         console.log('error: ', err)
+//       }
+//     }
+//   }
 
-  return (
-    <div className='h-screen flex flex-col justify-center items-center' >
-      <button onClick={requestAccount} >Request accounts</button>
-      <button onClick={fetchGreeting} className='p-1 border-2 border-gray-400 rounded' >Fetch Greeting</button>
-      <button onClick={setGreeting} className='p-1 border-2 border-gray-400 rounded' >Set Greeting</button>
-      <input
-        type="text"
-        onChange={e => setGreetingValue(e.target.value)}
-        placeholder='set greeting'
-        value={greeting}
-        className='border-2 border-gray-400'
-      />
-    </div>
-  )
-}
+//   const setGreeting = async () => {
+//     if (!greeting) return
+//     if (typeof window.ethereum !== undefined) {
+//       await requestAccount()
+//       const provider = new ethers.providers.Web3Provider(window.ethereum)
+//       const signer = provider.getSigner()
+//       const contract = new ethers.Contract(greeterAddress, Greeter.abi, signer) // create a transaction with signer (pass in signer instead of provider like above)
+//       const transaction = await contract.setGreeting(greeting)
+//       setGreetingValue('')
+//       await transaction.wait() // wait for transaction to confirm on blockchain
+//       fetchGreeting()
+//     }
+//   }
 
-export default HomePage
+//   return (
+//     <div className='h-screen flex flex-col justify-center items-center' >
+//       <button onClick={requestAccount} >Request accounts</button>
+//       <button onClick={fetchGreeting} className='p-1 border-2 border-gray-400 rounded' >Fetch Greeting</button>
+//       <button onClick={setGreeting} className='p-1 border-2 border-gray-400 rounded' >Set Greeting</button>
+//       <input
+//         type="text"
+//         onChange={e => setGreetingValue(e.target.value)}
+//         placeholder='set greeting'
+//         value={greeting}
+//         className='border-2 border-gray-400'
+//       />
+//     </div>
+//   )
+// }
+
+// export default HomePage
--- scripts/holland-token-script.js
+++ scripts/holland-token-script.js
@@ -11,14 +11,14 @@ async function main() {
   console.log("HollandToken deployed to:", hollandToken.address)
 
   // get contract from artifacts
-  // const artifactsHOLPath = path.join(__dirname, '../artifacts/contracts/HollandToken.sol/HollandToken.json')
-  // const compiledContract = JSON.parse(fs.readFileSync(artifactsHOLPath))
+  const artifactsHOLPath = path.join(__dirname, '../artifacts/contracts/HollandToken.sol/HollandToken.json')
+  const compiledContract = JSON.parse(fs.readFileSync(artifactsHOLPath))
 
   // copy contract/contract address to public directory
-  // const publicHOLPath = path.join(__dirname, "../public/HollandToken.json")
-  // const publicHOLAddressPath = path.join(__dirname, "../public/HollandTokenAddress.json")
-  // fs.writeFileSync(publicHOLPath, JSON.stringify(compiledContract));
-  // fs.writeFileSync(publicHOLAddressPath, JSON.stringify({ "address": hollandToken.address }))
+  const publicHOLPath = path.join(__dirname, "../public/HollandToken.json")
+  const publicHOLAddressPath = path.join(__dirname, "../public/HollandTokenAddress.json")
+  fs.writeFileSync(publicHOLPath, JSON.stringify(compiledContract));
+  fs.writeFileSync(publicHOLAddressPath, JSON.stringify({ "address": hollandToken.address }))
 }
 
 main()
