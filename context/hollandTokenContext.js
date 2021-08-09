51fb83ccccc112e9ea8b946a898547b11a030a60
Holland Pleskac <hollandpleskac@gmail.com>
2021-08-09 11:16:40 -0700

    new deployment

--- context/hollandTokenContext.js
+++ context/hollandTokenContext.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 
-// const url = 'http://localhost:3000/'
+// const url = 'http://localhost:3000/' 
 const url = 'https://holland-tokens-kymlnzjws-hollandpleskac.vercel.app/'
 
 const HollandTokenContext = React.createContext({
